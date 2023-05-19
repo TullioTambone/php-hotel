0cdacd3af3937421098b524527c379ad43a792d2
TullioTambone <tulliotambone97@gmail.com>
2023-05-19 12:40:22 +0200

    bootstrap

--- index.php
+++ index.php
@@ -39,5 +39,28 @@
         ],
 
     ];
-
+    // Stampare tutti i nostri hotel con tutti i dati disponibili.
+    // Iniziate in modo graduale.
+    // Prima stampate in pagina i dati, senza preoccuparvi dello stile.
+    // Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+    // Bonus:
+    // 1 - Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+    // 2 - Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
+    // NOTA: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
+    // Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel.
 ?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    </head>
+    <body>
+        
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    </body>
+</html>
