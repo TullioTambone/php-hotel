e5e1479d10c4097462d9926b546c3575df035ad2
TullioTambone <tulliotambone97@gmail.com>
2023-05-19 15:25:43 +0200

    bonus 1 e 2 -- fine esercizio

--- index.php
+++ index.php
@@ -47,6 +47,15 @@
         ]
     ];
 
+    $parcheggio = $_GET['parcheggio'];
+    $voto = $_GET['voto'];
+
+    $filtroVoto = array_filter($hotels, function ($element) use ($voto, $parcheggio) {
+        if ($element['vote'] >= $voto && $element['parking'] == ($parcheggio === 'true')) {
+            return true;
+        }
+        return false;
+    });
 ?>
 
 <!doctype html>
@@ -58,6 +67,32 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     </head>
     <body>
+    
+    <form action="index.php" method="GET">
+        <div class="mb-3 p-3 d-flex">
+            <div>
+                <label for="trovaParcheggio">includere il parcheggio?</label>
+                <select name="parcheggio" id="trovaParcheggio" class="d-block">
+                    <option selected="true" value="">tutti gli hotels</option>
+                    <option value="true">SI</option>
+                    <option value="false">NO</option>
+                </select>
+            </div>
+            <div class="mx-3">
+                <label for="">scegliere per voto</label>
+                <select name="voto" id="" class="d-block">
+                    <option selected='true' value="">tutti gli hotels</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary">SUBMIT</button>
+        </div>
+    </form>
+
     <table class="table">
         <thead>
             <tr>
@@ -68,20 +103,34 @@
                 <th scope="col">distanza dal centro</th>
             </tr>
         </thead>
-            <tbody>
-                <?php foreach($hotels as $hotel){
+        <tbody>
 
-
-                    echo "<tr>" .
-                        "<td>" . $hotel['name'] . "</td>" .
-                        "<td>" . $hotel['description'] . "</td>" .
-                        "<td>" . ($hotel['parking'] ? 'presente' : 'non Presente') . "</td>" . 
+            <?php if ($voto == '' && $parcheggio == '') {
+                foreach ($hotels as $hotel){
+                    echo "<tr>" . 
+                        "<td>" . $hotel['name'] . "</td>" . 
+                        "<td>" . $hotel['description'] . "</td>" . 
+                        "<td>" . ($hotel['parking'] ? 'presente' : 'non presente') . "</td>" . 
                         "<td>" . $hotel['vote'] . "</td>" . 
-                        "<td>" . $hotel['distance_to_center'] . "</td>"
-                    . "</tr>";
-                } ?>
-            </tbody>
-        </table>
+                        "<td>" . $hotel['distance_to_center'] . "</td>" . 
+                    "</tr>";
+                };
+            } else{
+
+                foreach ($filtroVoto as $hotel) {
+                    echo "<tr>" . 
+                        "<td>" . $hotel['name'] . "</td>" . 
+                        "<td>" . $hotel['description'] . "</td>" . 
+                        "<td>" . ($hotel['parking'] ? 'presente' : 'non presente') . "</td>" . 
+                        "<td>" . $hotel['vote'] . "</td>" . 
+                        "<td>" . $hotel['distance_to_center'] . "</td>" . 
+                    "</tr>";
+                };
+            }
+            ?>
+        </tbody>
+    </table>
+
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     </body>
